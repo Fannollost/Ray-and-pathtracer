9158e773f2669d71bc29ac186acea6c26dd20394
Yannick Visser <y.visser4@students.uu.nl>
2022-11-23 15:07:53 +0100

    diffuse material working, anti-aliassing, beginning of recursion

--- renderer.cpp
+++ renderer.cpp
@@ -15,37 +15,34 @@ void Renderer::Init()
 // -----------------------------------------------------------
 float3 Renderer::Trace(Ray& ray, int depth)
 {
-	if (depth <= 0) return float3(0, 0, 0);
+	if (depth <= 0) return float3(1, 1, 1);
 
-	float t_min = 0.00001f;
+	float t_min = 0.001f;
 	scene.FindNearest(ray, t_min);
 	if (ray.objIdx == -1) return 0; // or a fancy sky color	
 
 	float3 totCol = float3(0);
 	float3 attenuation;
+	material* m = ray.GetMaterial();
+	Ray scattered;
+	float3 I = ray.O + ray.t * ray.D;
+	float3 N = scene.GetNormal(ray.objIdx, I, ray.D);
 	for(int i = 0; i < sizeof(scene.light) / sizeof(scene.light[0]); i++){
-		float3 I = ray.O + ray.t * ray.D;
-		float3 N = scene.GetNormal(ray.objIdx, I, ray.D);
-		material m = *ray.GetMaterial();
-		
+	
 		float3 lightRayDirection = scene.light[i]->GetLightPosition() - ray.IntersectionPoint();
 
 		float len = length(lightRayDirection);
 		Ray r = Ray(ray.IntersectionPoint(), normalize(lightRayDirection), ray.color, len);
-		if (scene.IsOccluded(r, t_min)) break;
+		if (scene.IsOccluded(r, t_min)) continue;
 
 		float str = dot(N, normalize(lightRayDirection));
 		if (str < 0.0f) str = 0.0f;
 		totCol += ray.color * scene.light[i]->GetLightColor() * str * scene.light[i]->GetLightIntensityAt(ray.IntersectionPoint());
-		Ray scattered;
-		if (m.scatter(ray, attenuation, scattered, N)) {
-			return attenuation * Trace(scattered, depth - 1);
-		}
-
-
 	}
-
-	return totCol;
+	if (m->scatter(ray, attenuation, scattered, N)) {
+		//totCol += ray.color * attenuation * scene.light[i]->GetLightColor() * str * scene.light[i]->GetLightIntensityAt(ray.IntersectionPoint());
+		return totCol * attenuation * Trace(scattered, depth -1);
+	}
 }
 	//float3 albedo = scene.GetAlbedo(ray.objIdx, I);
 
@@ -89,9 +86,15 @@ void Renderer::Tick( float deltaTime )
 	for (int y = 0; y < SCRHEIGHT; y++)
 	{
 		// trace a primary ray for each pixel on the line
-		for (int x = 0; x < SCRWIDTH; x++)
-			accumulator[x + y * SCRWIDTH] =
-				float4( Trace( camera.GetPrimaryRay( x, y ) , 1), 10 );
+		for (int x = 0; x < SCRWIDTH; x++) {
+			float3 totCol = float3(0);				//antialiassing
+			for (int s = 0; s < scene.aaSamples; ++s) {
+				float newX = (x + RandomFloat());
+				float newY = (y + RandomFloat());
+				totCol += Trace(camera.GetPrimaryRay(newX, newY), 1);
+			}
+			accumulator[x + y * SCRWIDTH] = totCol / scene.aaSamples;
+		}
 		// translate accumulator contents to rgb32 pixels
 		for (int dest = y * SCRWIDTH, x = 0; x < SCRWIDTH; x++)
 			screen->pixels[dest + x] = 
